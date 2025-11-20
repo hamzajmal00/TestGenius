a8d6f51795d8a1331479ff2f526c29fef5aae23f
hamza ajmal <tempmanagebyhamza@gmail.com>
2025-11-21 00:05:49 +0500

    tess

--- fdfd.js
+++ fdfd.js
@@ -1,12 +1,5 @@
-function solution() {
-  1 + 2;
-  return d;
-}
-diff dfffdfdfsdasdsaddd(){
-    return 42;
-}
-dfdfs 
-fdsfdsf
-function anotherFunction() {
-    return "Hello, World!";
+try {
+  fdfdfd;
+} catch (error) {
+  dfdfdfdfdf;
 }
